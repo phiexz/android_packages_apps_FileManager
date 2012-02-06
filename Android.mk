badb5d1afc12427cef8d4fadb8816eb53d399933
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2012-02-06 21:02:54 +0700

    fix build and works :D

--- Android.mk
+++ Android.mk
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# works on Galaxy Mini
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
